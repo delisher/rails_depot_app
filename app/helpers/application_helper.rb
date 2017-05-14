0f5e297823ef184c46f70609b08487cc2ca5ced9
Sergey Smirnov <crazyjent@gmail.com>
2017-05-15 00:45:20 +0300

    ajax cart + blind effects (11)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	def hidden_div_if(condition, attributes = {}, &block)
+		if condition
+			puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+			attributes["style"] = "display: none"
+		else
+			puts "-----------------"
+		end
+		content_tag("div", attributes, &block)
+	end
 end
